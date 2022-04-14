0019e9ad3463ad90e4ef2856b89e2c4dc95ce861
rubyqureshi <83620864+rubyqureshi@users.noreply.github.com>
2022-04-14 22:11:10 +1200

    Update proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -668,16 +668,16 @@ showprocs(void){
 	[ZOMBIE]    "zombie"
 	};
 	char *state;
-	int old_id=0; int old_addr=0; int parent_id=0;
+	long old_id=0; long old_addr=0; long parent_id=0;
 	printf("ID \t Process Addr \t ParentID Addr \t State \t Name \t Size(bytes) \t \n");
 	for (p = proc; p < &proc[NPROC]; p++){
 		acquire(&wait_lock);
 		acquire(&p->lock);
-		if((int) p->parent==old_addr){
+		if( (int) p->parent==old_addr){
 			parent_id = old_id;
 		}
 		else{
-			parent_id = (int) p->parent;
+			parent_id = (long) p->parent;
 		}
 		
 		//struct proc *parent_p = p->parent;
@@ -686,7 +686,7 @@ showprocs(void){
 			printf("%d \t %d \t %d \t \t %s \t %s \t %d \n" , p->pid,p, p->parent,state, p->name,p->sz);
 			printf("%d \t %d \t %d \t \t %s \t %s \t %d \n" , p->pid,p, parent_id,state, p->name,p->sz);
 			count++;
-			old_id=p->pid; old_addr=p;
+			old_id=p->pid; old_addr= (long) p;
 		}
 		release(&p->lock);
 		release(&wait_lock);
