c3ee9d0d9f851f30a3b1a6c66beeea399e3052b9
rubyqureshi <83620864+rubyqureshi@users.noreply.github.com>
2022-04-14 19:16:46 +1200

    Update proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -664,7 +664,7 @@ showprocs(void){
 		
 		acquire(&p->lock);
 		if(p->state != UNUSED)
-			printf("%d %d %s %s %d" , p->pid, p->parent,SLEEP, p->name,sizeof(p->name));
+			printf("%d %d %s %s %d" , p->pid, p->parent,"SLEEP", p->name,sizeof(p->name));
 		release(&p->lock);
 	}
 	
