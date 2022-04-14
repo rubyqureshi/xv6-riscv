bbf17640fa2288d849d493e19d3ba4c2e4b10d9d
rubyqureshi <83620864+rubyqureshi@users.noreply.github.com>
2022-04-14 17:49:31 +1200

    Update ps.c

--- user/ps.c
+++ user/ps.c
@@ -1,7 +1,7 @@
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "user/user.h"
-#include "kernel/fcntl.h"
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
 
 void main()
 {
