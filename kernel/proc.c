0460c6749f271f89d4449328a0a840b0d8806866
rubyqureshi <83620864+rubyqureshi@users.noreply.github.com>
2022-04-14 22:17:02 +1200

    Final updates to make spacing the same

--- kernel/proc.c
+++ kernel/proc.c
@@ -669,7 +669,7 @@ showprocs(void){
 	};
 	char *state;
 	long old_id=0; long old_addr=0; long parent_id=0;
-	printf("PID \t PPID  \t  State \t \t Name \t Size(bytes) \t \n");
+	printf("PID \t PPID \t State \t Name \t Size(bytes) \t \n");
 	for (p = proc; p < &proc[NPROC]; p++){
 		acquire(&wait_lock);
 		acquire(&p->lock);
@@ -681,7 +681,7 @@ showprocs(void){
 		}
 		if(p->state != UNUSED){			
 			state = states[p->state];
-			printf("%d \t %d \t \t %s \t %s \t %d \n" , p->pid, parent_id,state, p->name,p->sz);
+			printf("%d \t %d \t %s \t %s \t %d \n" , p->pid, parent_id,state, p->name,p->sz);
 			count++;
 			old_id=p->pid; old_addr= (long) p;
 		}
