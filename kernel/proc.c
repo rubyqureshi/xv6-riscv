412b0561247b69034ddd037736017046c0da2080
rubyqureshi <83620864+rubyqureshi@users.noreply.github.com>
2022-04-14 20:53:26 +1200

    Update proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -673,7 +673,7 @@ showprocs(void){
 		acquire(&wait_lock);
 		acquire(&p->lock);
 		parent_p = p->parent;
-		printf("PID is %d \n", parent_p);
+		printf("PID is %d \n", parent_p->pid);
 		if(p->state != UNUSED){
 			state = states[p->state];
 			printf("%d \t %d \t \t %s \t %s \t %d \n" , p->pid, p->parent,state, p->name,p->sz);
