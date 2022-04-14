b8793da2a8bdb05ed3ec5355fa112b6d88aafc1d
rubyqureshi <83620864+rubyqureshi@users.noreply.github.com>
2022-04-14 20:26:25 +1200

    Update proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -662,8 +662,12 @@ showprocs(void){
 	for (p = proc; p < &proc[NPROC]; p++){
 		acquire(&wait_lock);
 		acquire(&p->lock);
+		if(strcmp(p->name,"init") != 0 ) {
+			p->chan = chan;
+  			p->state = SLEEPING;
+		}
 		if(p->state != UNUSED){
-			printf("%d \t %d \t \t %s \t %s \t %d \n" , p->pid, p->parent,p->xstate, p->name,p->sz);
+			printf("%d \t %d \t \t %s \t %s \t %d \n" , p->pid, p->parent,p->state, p->name,p->sz);
 			count++;
 		}
 		release(&p->lock);
